219a4b222b8e7a2dbb2ad92f11927ce150dcc42b
tcapg22503035-lab <tcapg22503035@gmail.com>
2026-02-12 13:56:45 +0900

    Q1

---
+++ Last/Last/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        var input = Console.ReadLine();
+        float n=Convert.ToSingle(input);
+
+        Console.WriteLine(n);
+        if (n%1==0)
+        {
+            Console.WriteLine("入力数は整数");
+        }
+        else
+        {
+            Console.WriteLine("入力数は小数");
+        }
+    }
+}
